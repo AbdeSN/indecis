131c8dba504f7b498e01b73f9013252c40a3443d
abderrahmane <hassaineabde@yahoo.fr>
2017-10-03 10:46:37 +0200

    add include footer and header

---
+++ page_connexion.php
@@ -0,0 +1,39 @@
+<?php?>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Portail d'authentification</title>
+
+    <!-- Bootstrap -->
+    <link href="assests/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!--CSS -->
+    <link rel="stylesheet" href="style/style.css">
+</head>
+<body>
+<?php include 'include/header.php';
+echo $param['message']?>
+
+<div class="container bandeau-connexion">
+
+    <h1 class="bandeau-connexion-h1">Connexion</h1>
+
+    <div class="formulaire">
+        <form action="" method="post">
+            <p class="form"><input type="text" name="login" placeholder="Login" ></p><br>
+            <p class="form"><input type="password" name="mdp" placeholder="Mot de passe"></p><br>
+            <input class="btn btn-danger btn-lg" type="submit" name="connexion" value="Connexion"/>
+            <input class="btn btn-danger btn-lg" type="submit" name="forgot" value="Mot de passe oublié"/>
+            <input class="btn btn-danger btn-lg" type="submit" name="inscription" value="Inscription"/>
+        </form>
+
+
+    </div>
+</div>
+
+<?php include("include/footer.php");?>
+</body>
+</html>
