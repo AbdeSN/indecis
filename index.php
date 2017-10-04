02a08a5843037d233873e64dc08d0272fb9829dc
abderrahmane <hassaineabde@yahoo.fr>
2017-10-04 20:09:04 +0200

    clear error due to navbar

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    require_once "config/database.php";
-    require_once "libs/Model.class.php";
-    require_once "libs/Bootstrap.class.php";
-    require_once "libs/Controller.class.php";
-    require_once "libs/View.class.php";
-    $app = new Bootstrap(); // pour instancier la class bootstrap
-
-?>
